519bb6b32fbdda3fb8b510d523634feea76895f2
Eli Davis <eli.davis1995@gmail.com>
2015-03-06 01:27:52 -0600

    WeatherGrabber Work in progress
    Attempting to pull "cross site scripting" with Weather Grabber.  The
    same method worked with reddit but is now throwing me the not the same
    domain error with forcast.com.  Kinda weird.  Looking into JSONP but
    it's probably not an option.  The only other thing we can do is force
    users to use proxies or set up our own server at this point, look into
    node.js If I can't find another weather resource.  Also those sentences
    produced on your phone are in the json file :P

--- WeatherGrabber.js
+++ WeatherGrabber.js
@@ -6,9 +6,38 @@ function Weather(){
 }
 
 function grabWeatherConditions(lat,longit){
-	console.log(lat + ", "+ longit);
 	
-	weather = new Weather()
+	//get weather info though
+	var link = "forecast.weather.gov/MapClick.php?lat="+lat+"&lon="+longit+"&FcstType=jsonp";
+	var info = getJSON(link);
+	
+	console.log(link+" , "+info);
+	
+	weather = new Weather();
 	weather.inOneWord = "Rainy";
+	
 	return weather;
+}
+
+function getJSON(link){
+	
+	var grabbedJason = "";
+	
+	$(document).ready(function(){
+				
+		$("#tempAjaxStorage").load(link, function(response, status, xhr){
+				if (status == "success"){
+					
+				} else {
+					$("#tempAjaxStorage").html("An error occured: <br>" + xhr.status + " " + xhr.statusText+"</br> This could have occured from you the browser being IE or some anti virus protection"+
+					"</br>Come on now we're not going to hurt you..")
+				}
+		});
+		//$("#tempAjaxStorage").html("shit");		
+				
+	});
+	
+	grabbedJason = document.getElementById("tempAjaxStorage").innerHTML;
+	
+	return grabbedJason;
 }
